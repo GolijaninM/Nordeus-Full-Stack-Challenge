7d54de1f565459ca4964b235c86b963dfc88fbab
Mihajlo Golijanin <golijaninmihajlo@gmail.com>
2026-04-26 18:47:54 +0200

    Fetch initial data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,20 @@ import MainMenu from './MainMenu'
 
 function App() {
 
-  const handleStartGame = () => {
-    console.log("Hello, World!");
+  const handleStartGame = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/start-run');
+      
+      if (!response.ok) {
+        throw new Error(`Server error! Status: ${response.status}`);
+      }
+      
+      const data = await response.json();
+      console.log(data);
+
+    } catch (error) {
+      console.error("Failed to fetch game data:", error);
+    }
   };
 
   return (
